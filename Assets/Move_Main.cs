24c5aa68367fd0b05364cda52b351df8d7b90166
joaopat98 <joaopat98@gmail.com>
2017-02-17 19:47:39 +0000

    Revert "Fix halted rotation"
    This reverts commit 4d82e2bcdde410ef91af1cb87234403961eb78dd.

--- Assets/Move_Main.cs
+++ Assets/Move_Main.cs
@@ -7,7 +7,6 @@ public class Move_Main : MonoBehaviour {
 	public float rot_speed;
 	Vector2 rt = new Vector2(1,0), lt = new Vector2(-1,0); 
 	Vector2 rot = new Vector2(0,0);
-	bool orient = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,20 +18,18 @@ public class Move_Main : MonoBehaviour {
 			transform.Translate (rt * mov_speed * Time.deltaTime, Space.World);
 			if (rot.y > 0)
 				rot += new Vector2(0,-rot_speed * Time.deltaTime);
-			orient = true;
 		}
-		else if (orient == true && rot.y > 0)
-			rot += new Vector2(0,-rot_speed * Time.deltaTime);
+		/*else if(rot.y > 0)
+			rot += new Vector2(0,-rot_speed * Time.deltaTime);*/
 
 
 		if (Input.GetKey (KeyCode.A) == true){
 			transform.Translate (lt * mov_speed * Time.deltaTime, Space.World);
 			if (rot.y < 180)
 				rot += new Vector2(0,rot_speed * Time.deltaTime);
-			orient = false;
 		}
-		else if (orient == false && rot.y < 180)
-			rot += new Vector2(0,rot_speed * Time.deltaTime);
+		/*else if(rot.y < 180)
+			rot += new Vector2(0,-rot_speed * Time.deltaTime);*/
 
 
 		if (rot.y < 0)

e19abb58bae007f7499468e485c11f7e32bc48fa
joaopat98 <joaopat98@gmail.com>
2017-02-18 12:06:29 +0000

    Changed ground detection to raycast, fixed stuck when leaning against wall

---
+++ Assets/R_test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y), Vector2.down, 0.1f))
+			print ("lel");
+	}
+}

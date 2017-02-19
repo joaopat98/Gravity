944b48c61a596cf65a3ec2b5e0b04736b1d1b054
joaopat98 <joaopat98@gmail.com>
2017-02-19 01:22:30 +0000

    Added moving animation

---
+++ Assets/Scripts/Sprite_anim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_anim : MonoBehaviour {
+	public Sprite[] sprites;
+	SpriteRenderer s_rend;
+	// Use this for initialization
+	void Start () {
+		s_rend = gameObject.GetComponent<SpriteRenderer> ();
+		s_rend.sprite = sprites [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Horizontal") != 0) {
+			if(Time.fixedTime % 0.1f <= 0.05f)
+				s_rend.sprite = sprites [1];
+			else
+				s_rend.sprite = sprites [2];
+		}
+		else
+			s_rend.sprite = sprites [0];
+	}
+}

c5c31d763b3a38921d3dc05c00a45621d5086a42
SPECTER\Luis David <luisdcb97@gmail.com>
2017-02-19 03:52:44 +0000

    Started UI overlay

---
+++ Assets/UI_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Script : MonoBehaviour {
+    private bool exists = false;
+
+    private Text level;
+
+    // Use this for initialization
+    void Start () {
+         level = this.GetComponent<Text>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        level.text = "LEVEL " + SceneManager.GetActiveScene().buildIndex;
+
+    }
+}

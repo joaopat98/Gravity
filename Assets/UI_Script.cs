da7f2f1d77131c95c194fb6d71ce812a96da5a11
SPECTER\Luis David <luisdcb97@gmail.com>
2017-02-19 04:41:16 +0000

    Organized sound and made overlay prefab
    Added key and door tags

--- Assets/UI_Script.cs
+++ Assets/UI_Script.cs
@@ -5,19 +5,44 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UI_Script : MonoBehaviour {
-    private bool exists = false;
+    private bool exists_audio = false;
 
     private Text level;
+    private AudioSource som_bg;
+
+    private int level_keys;
+    private int current_keys = 0;
 
     // Use this for initialization
     void Start () {
-         level = this.GetComponent<Text>();
+        level = this.GetComponentInChildren<Text>();
         
+        if (exists_audio == false)
+        {
+            som_bg = this.GetComponent<AudioSource>();
+            som_bg.Play();
+            exists_audio = true;
+            DontDestroyOnLoad(this);
+
+            level_keys = (GameObject.FindGameObjectsWithTag("Key")).Length;
+
+
+        }
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         level.text = "LEVEL " + SceneManager.GetActiveScene().buildIndex;
+
+
+        //GameObject son = this.GetComponentInChildren<RectTransform>().gameObject;
+        //son.GetComponentInChildren<Text>().text = 3.ToString();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene(2);
+        }
+
 
     }
 }

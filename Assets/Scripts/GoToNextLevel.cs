e023a963fe431687d17c76fec2a93f4314523cce
SPECTER\Luis David <luisdcb97@gmail.com>
2017-02-19 03:03:26 +0000

    Adds script to go to next level

---
+++ Assets/Scripts/GoToNextLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

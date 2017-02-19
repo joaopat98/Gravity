0f9765b210c0d9c165e42db37355a35e7afef94c
SPECTER\Luis David <luisdcb97@gmail.com>
2017-02-19 01:17:38 +0000

    Adds confirmation screen when choosing levels

---
+++ Assets/Scripts/MainMenuButtons/LoadSceneOnClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public Canvas button_canvas;
+    public Canvas confirm_canvas;
+    private bool confirming = false;
+    private int index_saved;
+
+    void Update()
+    {
+        if (confirming == true)
+        {
+            Debug.Log("inside");
+            if(Input.GetButton("Submit") == true)
+            {
+                SceneManager.LoadScene(index_saved);
+            }
+            else if(Input.GetButton("Cancel") == true)
+            {
+                confirming = false;
+                confirm_canvas.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    public void LoadByIndex(int index)
+    {
+        
+        confirm_canvas.gameObject.SetActive(true);
+
+        Text painel = confirm_canvas.GetComponentInChildren<Text>();
+
+        painel.text = "Deseja continuar para o nivel " + index + "?\n\n\nPressione [Submit] para continuar\nPressione [Cancel] para cancelar";
+        index_saved = index;
+        confirming = true;
+    }
+    
+}
